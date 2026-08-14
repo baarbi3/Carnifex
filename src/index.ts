12b7dfafde465eacdac5fff387651ef0971787b1
baarbi3 <ayan.whiz@gmail.com>
2026-08-14 14:15:18 +0500

    warning fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import { loadCommands } from "./utils/commandHandler";
 import { loadAutomod } from "./automod";
+import { configDotenv } from "dotenv";
+
+configDotenv({
+    path: "./.env"
+});
+
+
 
 // Defining a fresh client
 const client = new Client({
